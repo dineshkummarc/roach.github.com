d1b9c0f41f57523bd85277c88d187e8ee4d5b4b0
Jason <urbandev@gmail.com>
2010-12-20 23:40:12 -0800

    adjusted glow temperature and enabled pulse

--- urbandev-home.js
+++ urbandev-home.js
@@ -15,14 +15,16 @@ $('#urbandev-logo').animate(
    1000,
    function(){
     $('#urbandev-logo .slogan').animate({top: [275, 'swing'], opacity: 0.7}, 500, function(){
-	    $('#urbandev-logo-glow').animate({opacity: 0.6},250).animate({opacity: 0.4},1000);
+		    $('#urbandev-logo-glow').animate({opacity: 0.2},150, function(){
+		    $('#urbandev-logo-glow').animate({opacity: 0.1},50).animate({opacity: 0.4},1000);			
+		});
 	    $('ul#menu').animate({top: -12},750);	
-      // var t=setInterval("logoPulse();",8000);
+      var t=setInterval("logoPulse();",8000);
     });
   }
 );      
 
-function logoPulse(){$('#urbandev-logo-glow').animate({opacity: 0.6},1500).animate({opacity: 0.4},4000);};
+function logoPulse(){$('#urbandev-logo-glow').animate({opacity: 0.2},500).animate({opacity: 0.4},1000);};
 
 // End animation functions
 

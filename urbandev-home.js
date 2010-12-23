bbb29ec64de811b6b67842f2742fb1108db31fe6
Jason <urbandev@gmail.com>
2010-12-23 03:29:50 -0800

    testing content loading functionality

--- urbandev-home.js
+++ urbandev-home.js
@@ -28,5 +28,20 @@ function logoPulse(){$('#urbandev-logo-glow').animate({opacity: 0.2},500).animat
 // End animation functions
 
 // Content slider
+$('#menu-wrap ul#menu li a').bind('click', function() {
+	var url = $(this).attr("href");
+	loadContent(url, 545);
+	return false
+});
 
+function loadContent(url, height){
+	$('#home-content').fadeOut('fast', function(){
+		$('#home-content').hide().addClass('hidde');
+		$('#page-container').animate({height: height},2500, function(){
+			$('#page-content').load(url, function() {
+				$('#page-content').fadeIn('fast');
+			});
+		})
+	})
+}
 // End content slider

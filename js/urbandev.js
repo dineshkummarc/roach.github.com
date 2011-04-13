a4310d815edaafc0f962e35dbefc0136ae07a8f7
Jason <urbandev@gmail.com>
2011-04-13 05:46:59 -0700

    urbandev.js: fixed whitespacing

--- js/urbandev.js
+++ js/urbandev.js
@@ -1,21 +1,21 @@
-var parseUri = function(str){var o=parseUri.options,m=o.parser[o.strictMode?"strict":"loose"].exec(str),uri={},i=14;while(i--)uri[o.key[i]]=m[i]||"";uri[o.q.name]={};uri[o.key[12]].replace(o.q.parser,function($0,$1,$2){if($1)uri[o.q.name][$1]=$2;});return uri;};parseUri.options={strictMode:false,key:["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],q:{name:"queryKey",parser:/(?:^|&)([^&=]*)=?([^&]*)/g},parser:{strict:/^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,loose:/^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/}};
+var parseUri = function (str) {var o = parseUri.options, m = o.parser[o.strictMode ? "strict" : "loose"].exec(str), uri = {}, i = 14; while (i--) {uri[o.key[i]] = m[i] || ""; } uri[o.q.name] = {}; uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {if ($1) {uri[o.q.name][$1] = $2; } }); return uri; }; parseUri.options = {strictMode: false, key: ["source", "protocol", "authority", "userInfo", "user", "password", "host", "port", "relative", "path", "directory", "file", "query", "anchor"], q: {name: "queryKey", parser: /(?:^|&)([^&=]*)=?([^&]*)/g}, parser: {strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/, loose: /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/}};
 
 var urbanDev = {
-	loadContent: function(URL){
+	loadContent: function (URL) {
 		var contentRequest = {};
 		contentRequest.page = parseUri(URL).file;
 		contentRequest.height = parseUri(URL).queryKey.height;
-		$('#page-container').stop(true,true);
-		$('#page-content').fadeOut('fast', function(){
+		$('#page-container').stop(true, true);
+		$('#page-content').fadeOut('fast', function () {
 			this.innerHtml = '';
 			$.ajax({
 				url: contentRequest.page,
-				success: function(result) {
+				success: function (result) {
 					$('#page-content').html(result);
 					urbanDev.page = contentRequest.page;
-					$('#page-container, #content-wrap').animate({height: contentRequest.height},500,
-						function(){$('#page-content').fadeIn('fast');}
-					);
+					$('#page-container, #content-wrap').animate({height: contentRequest.height}, 500,
+						function () {$('#page-content').fadeIn('fast'); }
+						);
 				},
 				error: function (request, status, error) {
 					// TODO: Add error handling.
@@ -24,18 +24,18 @@ var urbanDev = {
 		});
 	}
 };
-$(document).ajaxStart(function(){
-	
-}).ajaxStop(function(){
-
+$(document).ajaxStart(function () {
+	// TODO: Show ajax loading message
+}).ajaxStop(function () {
+	// TODO: Hide ajax loading message
 });
 
-$('a.local-content').live('click', function(event){
+$('a.local-content').live('click', function (event) {
 	urbanDev.loadContent(this.href);
 	event.preventDefault();
 });
 
-$('a.external-link').live('click', function(event){
-	window.open(this.href,'_blank');
+$('a.external-link').live('click', function (event) {
+	window.open(this.href, '_blank');
 	event.preventDefault();
 });

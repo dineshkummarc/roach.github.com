cfbf662ae26245dfb1423eff478eb50c2fa8ef22
Jason <urbandev@gmail.com>
2011-04-21 00:26:26 -0700

    index.html: changed local link URLs, updated urbanDev.initialize call urbandev.js: updates initialize functions and anchor change binding

--- js/urbandev.js
+++ js/urbandev.js
@@ -1,29 +1,36 @@
 var parseUri = function (str) {var o = parseUri.options, m = o.parser[o.strictMode ? "strict" : "loose"].exec(str), uri = {}, i = 14; while (i--) {uri[o.key[i]] = m[i] || ""; } uri[o.q.name] = {}; uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {if ($1) {uri[o.q.name][$1] = $2; } }); return uri; }; parseUri.options = {strictMode: false, key: ["source", "protocol", "authority", "userInfo", "user", "password", "host", "port", "relative", "path", "directory", "file", "query", "anchor"], q: {name: "queryKey", parser: /(?:^|&)([^&=]*)=?([^&]*)/g}, parser: {strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/, loose: /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/}};
 
 var urbanDev = {
+	initialize: function () {
+		var URL = window.location.href;
+		var homeUrl = $('#menu-wrap #menu .home-link').attr('href');
+		if (parseUri(URL).anchor) { urbanDev.loadContent(URL); } else { urbanDev.loadContent(homeUrl); }
+	},
 	loadContent: function (URL) {
+		var pages = { home: { file: "home.html", height: 420 }, services: { file: "services.html", height: 420 }, projects: { file: "projects.html", height: 420 }, portfolio: { file: "portfolio.html", height: 620 }, contact: { file: "contact.html", height: 620 } };
 		var contentRequest = {};
-		contentRequest.page = parseUri(URL).file;
-		contentRequest.height = parseUri(URL).queryKey.height;
-		$('#page-container').stop(true, true);
-		$('#page-content').fadeOut('fast', function () {
-			this.innerHtml = '';
-			contentDiv = this;
-			$.ajax({
-				url: contentRequest.page,
-				success: function (result) {
-					urbanDev.currentpage = contentRequest.page;
-					$(contentDiv).html(result);
-					console.log(contentDiv.innerHtml);
-					$('#page-container, #content-wrap').animate({height: contentRequest.height}, 500,
-						function () { $(contentDiv).fadeIn('fast'); }
-						);
-				},
-				error: function (request, status, error) {
-					// TODO: Add error handling.
-				}
+		if (pages[parseUri(URL).anchor]) {
+			contentRequest.page = pages[parseUri(URL).anchor].file;
+			contentRequest.height = pages[parseUri(URL).anchor].height;
+			$('#page-container').stop(true, true);
+			$('#page-content').fadeOut('fast', function () {
+				this.innerHtml = '';
+				contentDiv = this;
+				$.ajax({
+					url: contentRequest.page,
+					success: function (result) {
+						urbanDev.currentpage = contentRequest.page;
+						$(contentDiv).html(result);
+						$('#page-container, #content-wrap').animate({height: contentRequest.height}, 500,
+							function () { $(contentDiv).fadeIn('fast'); }
+							);
+					},
+					error: function (request, status, error) {
+						// TODO: Add error handling.
+					}
+				});
 			});
-		});
+		}
 	}
 };
 $(document).ajaxStart(function () {
@@ -59,9 +66,8 @@ urbanDev.logo = {
 	pulse: function () {$('.home-urbandev-logo-glow').animate({opacity: 0.2}, 500).animate({opacity: 0.4}, 1000); }
 };
 
-$('a.local-content').live('click', function (event) {
-	urbanDev.loadContent(this.href);
-	event.preventDefault();
+$(window).bind('hashchange', function (event) {
+	urbanDev.loadContent(window.location.href);
 });
 
 $('a.external-link').live('click', function (event) {
